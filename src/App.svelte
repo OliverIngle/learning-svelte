85c3fe14e5a8920bf0fb26e00dd1c365e79b369c
Oliver-ingle <oliver.ingle@gmail.com>
2022-01-17 09:26:19 +0100

    Project setup

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+	import Button from "./components/Button.svelte";
+
+	export let name: string;
+</script>
+
+<Button></Button>
+
+<style>
+</style>
---
+++ src/components/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+    let numbers: number[] = [1, 2]
+
+    function addNum() {
+        numbers = [...numbers, numbers.length + 1]
+    }
+
+    $: sum = numbers.reduce((acc, curr) => {
+        return acc + curr
+    })
+
+</script>
+
+<button on:click={addNum}>{numbers.join(' + ')}</button>
+<h1>{sum}</h1>
